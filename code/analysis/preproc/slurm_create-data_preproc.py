3fcce4c7ef10874fe03d5aeacf76e8385dfae1e4
isaacrc <isaacrc@princeton.edu>
2025-05-21 21:24:04 -0400

    updated preproc

--- code/analysis/preproc/slurm_create-data_preproc.py
+++ code/analysis/preproc/slurm_create-data_preproc.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-#python resample.py $IMAGE_TO_RESAMPLE $REFERENCE_IMAGE
-
+# # This code preprocesses data
 
 # In[2]:
 
@@ -19,17 +15,6 @@
 import nibabel as nib
 
 from nilearn.input_data import NiftiMasker , MultiNiftiMasker
-
-
-# In[4]:
-
-
-import nilearn as nil
-
-
-# In[5]:
-
-
 import numpy as np 
 import os
 import os.path
@@ -41,10 +26,7 @@ from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import PredefinedSplit
 from copy import deepcopy
-
-
-# In[6]:
-
+import nilearn as nil
 
 import warnings
 import sys  
@@ -91,10 +73,6 @@ from utils import sherlock_dir
 
 random.seed(10)
 
-
-# In[7]:
-
-
 #%matplotlib inline
 from brainiak import image, io
 from scipy.stats import stats
@@ -102,10 +80,6 @@ import nibabel as nib
 import numpy as np
 from matplotlib import pyplot as plt
 from brainiak import image, io
-
-
-# In[8]:
-
 
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
@@ -117,16 +91,6 @@ from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GroupShuffleSplit
 from sklearn.model_selection import LeavePGroupsOut
-
-
-# In[9]:
-
-
-import pandas as pd
-
-
-# In[10]:
-
 
 # Import machine learning libraries
 from nilearn.input_data import NiftiMasker
@@ -143,44 +107,7 @@ from sklearn.metrics import roc_auc_score
 import statistics
 
 
-# In[11]:
-
-
-# Visualize it as an ROI
-from nilearn.plotting import plot_roi
-#plot_roi(x)
-
-
-# In[12]:
-
-
-from nilearn.image import concat_imgs, resample_img, mean_img
-from nilearn.plotting import view_img
-
-
-# In[13]:
-
-
-from nilearn import datasets, plotting
-from nilearn.input_data import NiftiSpheresMasker
-
-from nilearn.glm.first_level import FirstLevelModel
-from nilearn.glm.first_level import make_first_level_design_matrix
-from nilearn.image import concat_imgs, resample_img, mean_img,index_img
-from nilearn import image
-from nilearn import masking
-from nilearn.plotting import view_img
-from nilearn.image import resample_to_img
-
-
 # # Functions 
-
-# In[14]:
-
-
-# Visualize it as an ROI
-from nilearn.plotting import plot_roi
-
 
 # In[15]:
 
@@ -242,203 +169,122 @@ def intersect_mask(sub, num_runs,reg, ses="ses-01",task="Attn"):
     return epi_data
 
 
-# In[ ]:
+# In[19]:
 
 
-
-
-
-# In[18]:
-
-
-def load_fMRI3d(sub, num_runs,reg, norm_type, ses="ses-01",task="Attn"):
-    # This is based off of 'load_data' function in template
-    # Loads all fMRI runs into a matrix #
+def load_fMRI3d(sub, num_runs, reg, norm_type, mask=None, ses="ses-01", task="Attn"):
     """
-    reg = T1 or MNI registration?
-    norm_type = by Space or by Time? - default is by space (rows)
-    """
-    concatenated_data = []
-    int_mask = intersect_mask(sub, 1,reg)
-    # 
-    for run in range(1, num_runs + 1):
-        if sub == "sub-002":
-            if run >=7:
-                run = run+1
-        if sub != "sub-010":
-            # Load epi data 
-            epi = load_epi_data(sub,ses,task,run,reg)
-        else:
-            # Load epi data 
-            print("sub-10, watch out")
-            bad_epi = load_epi_data(sub,ses,task,run,reg)
-            good_epi = load_epi_data("sub-001",ses,task,run,reg)
-            epi = resample_to_img(bad_epi , good_epi, interpolation='nearest')
-            int_mask = resample_to_img(int_mask , good_epi, interpolation='nearest')
-        # delete first 9 TRs
-        epi = index_img(epi,slice(9,209))
-        
-        # load confounds
-        run_conf = np.asarray(pd.read_csv(os.path.join(confounds + sub + "/func/", 
-                                                           '%s_ses-01_task-Attn_run-%s_desc-model_timeseries.csv') % (sub, run)))
-        print(run_conf.shape)
-        # clean image
-        # low_pass= .1,
-        clean_bold = image.clean_img(epi, standardize = False, confounds = run_conf[9:], high_pass=1/128, 
-                                   t_r=1.5, mask_img = int_mask)
-        
-        
-        #Smooth
-        clean_bold = image.smooth_img(clean_bold, fwhm=5)
-        
-        # F*k it mask off -- Load ROI data
-        #roi_samp =load_roi_mask(ROI_name,reg)
-        # Pull voxels from epi data # *** may need to change this 
-        #nifti_masker = NiftiMasker(mask_img=roi_samp)
-        #masked_data = nifti_masker.fit_transform(clean_bold)
-        
-        #append to cat_dat
-        concatenated_data.append(clean_bold)
-    "FINISHED YAY BEAST"
-    return concatenated_data, epi
+    Load and preprocess all fMRI runs into a list of 3D image volumes.
 
+    Parameters:
+    - sub (str): Subject ID (e.g., "sub-001")
+    - num_runs (int): Number of runs to load
+    - reg (str): Registration type ("T1" or "MNI")
+    - norm_type (str): Normalization method ("Space" or "Time")
+    - mask (Nifti1Image): Brain mask to apply
+    - ses (str): Session ID (default "ses-01")
+    - task (str): Task name (default "Attn")
 
-# In[27]:
-
-
-def load_fMRI3d(sub, num_runs,reg, norm_type, mask, ses="ses-01",task="Attn"):
-    # This is based off of 'load_data' function in template
-    # Loads all fMRI runs into a matrix #
-    """
-    reg = T1 or MNI registration?
-    norm_type = by Space or by Time? - default is by space (rows)
+    Returns:
+    - list of Nifti1Image: Cleaned and concatenated fMRI runs
     """
     concatenated_data = []
-    
+
     for run in range(1, num_runs + 1):
-        if sub == "sub-002":
-            if run >=7:
-                run = run+1
+        run_to_load = run
+        if sub == "sub-002" and run >= 7:
+            run_to_load += 1
+
         if sub != "sub-010":
-            # Load epi data 
-            epi = load_epi_data(sub,ses,task,run,reg)
+            epi = load_epi_data(sub, ses, task, run_to_load, reg)
         else:
-            # Load epi data 
-            print("sub-10, watch out")
-            bad_epi = load_epi_data(sub,ses,task,run,reg)
-            good_epi = load_epi_data("sub-001",ses,task,run,reg)
-            epi = resample_to_img(bad_epi , good_epi, interpolation='nearest')
-        # delete first 9 TRs ** change CONFOUNDS AS WELL
-        epi = index_img(epi,slice(5,209))
-        
-        # load confounds
-        run_conf = np.asarray(pd.read_csv(os.path.join(confounds + sub + "/func/", 
-                                                           '%s_ses-01_task-Attn_run-%s_desc-model_timeseries.csv') % (sub, run)))
-        print(run_conf.shape)
-        # clean image
-        # low_pass= .1,
-        clean_bold = image.clean_img(epi, standardize = False, confounds = run_conf[5:], high_pass=1/128, 
-                                   t_r=1.5, mask_img = mask)
-        
-        
-        #Smooth
-        clean_bold = image.smooth_img(clean_bold, fwhm=5)
-        
-        # F*k it mask off -- Load ROI data
-        #roi_samp =load_roi_mask(ROI_name,reg)
-        # Pull voxels from epi data # *** may need to change this 
-        #nifti_masker = NiftiMasker(mask_img=roi_samp)
-        #masked_data = nifti_masker.fit_transform(clean_bold)
-        
-        #append to cat_dat
-        concatenated_data.append(clean_bold)
-    "FINISHED YAY BEAST"
-    return concatenated_data, epi
+            print("sub-010, watch out")
+            bad_epi = load_epi_data(sub, ses, task, run_to_load, reg)
+            good_epi = load_epi_data("sub-001", ses, task, run_to_load, reg)
+            epi = resample_to_img(bad_epi, good_epi, interpolation='nearest')
+
+        # Remove first 5 TRs
+        epi = index_img(epi, slice(4, 210))
+        print(f"Run {run}: {epi.shape}")
+
+        # Load confounds
+        conf_path = os.path.join(
+            confounds, sub, "func",
+            f"{sub}_ses-01_task-Attn_run-{run_to_load}_desc-model_timeseries.csv"
+        )
+        run_conf = np.asarray(pd.read_csv(conf_path))
+
+        # Optional preprocessing (commented out)
+        # clean_epi = image.clean_img(
+        #     epi, t_r=1.5, low_pass=0.1, high_pass=1/128,
+        #     standardize=False, mask_img=mask
+        # )
+
+        # Optional smoothing
+        # clean_epi = image.smooth_img(clean_epi, fwhm=5)
+
+        # Optional ROI masking
+        # roi_mask = load_roi_mask(ROI_name, reg)
+        # nifti_masker = NiftiMasker(mask_img=roi_mask)
+        # masked_data = nifti_masker.fit_transform(clean_epi)
+
+        concatenated_data.append(epi)
+
+    print("FINISHED loading all runs.")
+    return concatenated_data
 
 
-# In[ ]:
+# # Dirs
 
-
-
-
-
-# # Define Static VARS
-
-# In[17]:
-
-
-"""
-To do:
-- log regression
-- optimize hyper parameters
-- what accuracies hsould i be expecting
-- only train and test on two
-- bootstrap
-- two splits, not four 
-- Try averagin datapoints 
-- motion
-- test on multiple subjects 
-- subject specific space 
-- implement z score during training
-"""
-
-
-# In[28]:
+# In[22]:
 
 
 ####
 data_dir = "/jukebox/graziano/coolCatIsaac/ATM/data/bids/derivatives/fmriprep/"
 rois_dir = "/jukebox/graziano/coolCatIsaac/ATM/data/work/rois/"
 behav_p = '/jukebox/graziano/coolCatIsaac/ATM/data/behavioral'
-sav_work = "/jukebox/graziano/coolCatIsaac/ATM/data/work/results/bpress_GLM/"
+sav_work = "/jukebox/graziano/coolCatIsaac/ATM/data/work/results/corr_data/"
 confounds = '/jukebox/graziano/coolCatIsaac/ATM/data/bids/derivatives/fmriprep/afni-head_mot/'
 workspace = "/jukebox/graziano/coolCatIsaac/ATM/data/work/workspace/"
 parc_dir = "/jukebox/graziano/coolCatIsaac/ATM/data/work/rois/schaef_par/MNI/"
 sav_fcma = '/jukebox/graziano/coolCatIsaac/ATM/data/work/workspace/load_fcma'
+sav_bpress = "/jukebox/graziano/coolCatIsaac/ATM/data/work/results/bpress_GLM/"
 
 
 # # lesss do it
 
-# In[29]:
+# In[23]:
 
 
 ##### GENERAL ####
-sub_list = ["sub-000","sub-001","sub-003","sub-004","sub-005","sub-006","sub-007","sub-008","sub-009",
-            "sub-011","sub-012","sub-013","sub-014"]
-sub_list = ["sub-000","sub-001","sub-005","sub-006","sub-007","sub-008","sub-009",
-            "sub-011","sub-012","sub-013","sub-014"]
-sub_list = ["sub-015", "sub-016","sub-017", "sub-018", "sub-019", "sub-020","sub-021", "sub-021"]
 sub_list = ["sub-000","sub-001","sub-002","sub-003","sub-004","sub-005","sub-006","sub-007","sub-008","sub-009",
             "sub-010","sub-011","sub-012","sub-013","sub-014","sub-015", "sub-016","sub-017", 
-            "sub-018", "sub-019", "sub-020","sub-021"]
-"""
-sub_list = ["sub-011","sub-012","sub-013","sub-014","sub-015", "sub-016","sub-017", 
-            "sub-018", "sub-019", "sub-020","sub-021"]
-"""
-#sub_list = ["sub-003"]
+            "sub-018", "sub-019", "sub-020","sub-021", "sub-022", "sub-023", "sub-024","sub-025","sub-026", "sub-027"]
 
 
 ###### LOADING VARS #######
 # Number of runs to load 
 num_runs = 10
 # Registration ust be either T1 or MNI
-reg = "MNI"
+reg = "MNI"# "MNI"
 # Registration Space # 
 norm_type = "space"
 # SUFFIX - change me
-suffix = "fwm5_mni_hp_nonorm_205"
+suffix = "205_noproc"
 
 
-# In[34]:
+# In[ ]:
 
 
 # LOAD group mask
 mask_file = os.path.join(sav_fcma, 'mask_10r_n22-subs.nii.gz')
+#mask_file = nib.load('/jukebox/graziano/coolCatIsaac/ATM/data/work/workspace/load_fcma/mask_10r_sub-001_t1.nii.gz')
 for sub in sub_list:
     print("POW, right in the kisser! Begin", sub)
     # Load all subject run data
-    concat, raw_epi = load_fMRI3d(sub, num_runs, reg, norm_type, mask_file)
-    np.save(sav_work + sub + "_"+ suffix + ".npy", concat, allow_pickle = True)
+    #concat, raw_epi = load_fMRI3d(sub, num_runs, reg, norm_type, mask_file)
+    concat = load_fMRI3d(sub, num_runs, reg, norm_type, mask_file)
+    np.save(sav_bpress + sub + "_"+ suffix + ".npy", concat, allow_pickle = True)
 print("phew, finished. go grab a cup of tea")
             
 
